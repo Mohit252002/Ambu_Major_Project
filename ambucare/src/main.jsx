a373cdb7d5bd79262f5c656c1f26316e8ea2d1e0
karan0094 <karanbishta104@gmail.com>
2023-12-27 17:16:49 +0530

    added components and removed not needed files added Routing

---
+++ ambucare/src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Home from './Components/Home/Home'
+import Layout from './Layout'
+import About from './Components/About/About'
+import { Route,RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+
+const router=createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path="" element={<Home/>}/>
+      <Route path='about/' element={<About/>}/>
+    </Route>
+  )
+)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
