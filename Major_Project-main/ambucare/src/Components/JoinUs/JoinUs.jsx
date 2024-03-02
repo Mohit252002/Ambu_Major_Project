1024b99cc5e0e1f9adb22c9279b6e2801d01f141
karan0094 <karanbishta104@gmail.com>
2024-03-02 09:49:41 +0530

    removing files

--- Major_Project-main/ambucare/src/Components/JoinUs/JoinUs.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function JoinUs() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
--- Major_Project-main/ambucare/src/Components/RegisterUser/RegisterUser.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function RegisterUser() {
-  return (
-    <div>
-      
-    </div>
-  )
-}
