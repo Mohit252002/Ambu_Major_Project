8985b5d1c9a548060b0d7b3eb4675169b6584165
karan0094 <karanbishta104@gmail.com>
2024-03-05 17:37:10 +0530

    added driver login and styled components

--- src/Components/DriverLogin/DriverLogin.jsx
+++ src/Components/DriverLogin/DriverLogin.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
-
-export default function DriverLogin() {
-  return (
-    <div>
-      
+import { NavLink } from 'react-router-dom';
+export default function DriverLogin(props) {
+  return(props.trigger) ? (
+   
+    <div className="card">
+    <div className="login">
+    <div className="write">
+        <p className="log">Login</p>
+        <p className="closebutton">  <button onClick={()=>props.setTrigger(false)}>X</button></p>
     </div>
-  )
+    <div className="userid">
+   
+    <input type="text" id="username" placeholder='Email /User Name'/>
+    <label for="username">User Name/Email</label>
+   
+   
+    </div>
+    <div className="pass">
+   
+    <input type="text" id="password"placeholder='Password'/>
+    <label htmlFor="password">Password</label>
+    </div>
+    <div className="Forgrem Remf">
+        <div className="Remember">
+        <input type="checkbox" id="remb" />
+        <label htmlFor="remb">Remember me</label>
+        </div>
+        <div className="forget">
+        <NavLink to="/forget" className="forgoption" style={({isActive})=>{return{ fontWeight:isActive?"bold":"",color:isActive?"purple":"",};}
+        }>forget Password?</NavLink>
+        </div>
+    </div>
+    <div className="button">
+        <button className='buttn'>LogIn</button>
+    </div>
+    </div>
+    </div>
+     
+  
+    ):
+    <>
+   
+  </>
+   
 }
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 import {Link,NavLink} from 'react-router-dom'
-// import Home from '../Home/Home';
+import DriverLogin from '../DriverLogin/DriverLogin'
 import Login from '../Login/Login'
 import './Head.css'
 function Header() {
   const[Visible,setVisible]=useState(false);
+  const[Dvisible,setDvisible]=useState(false);
   return (
     <>
     <div className="navbar">
@@ -18,12 +19,10 @@ function Header() {
         }>Home </NavLink>
         
         </li>
-        <li> <NavLink to="aboutUs"  className="navoption"style={({isActive})=>{return{ fontWeight:isActive?"bold":""};}
-        }>AboutUs </NavLink></li>
+       
         <li> <NavLink to="/JoinUs" className="navoption" style={({isActive})=>{return{ fontWeight:isActive?"bold":""};}
         }>JoinUs </NavLink></li>
-        <li> <NavLink to="/Login" className="navoption" style={({isActive})=>{return{ fontWeight:isActive?"bold":"",color:isActive?"#C70039":"",};}
-        }>Login </NavLink></li>
+        
         
         <li> <NavLink to="/Services" className="navoption" style={({isActive})=>{return{ fontWeight:isActive?"bold":"",color:isActive?"#C70039":"",};}
         }>Services </NavLink></li>
@@ -31,7 +30,14 @@ function Header() {
 <li> <NavLink to="/RegisterUser" className="navoption" style={({isActive})=>{return{ fontWeight:isActive?"bold":"",color:isActive?"#C70039":"",};}
         }>Register </NavLink></li>
         <li>
-          <button onClick={()=>setVisible(true)}>Login</button>
+          <button className="Driverl" onClick={()=>setDvisible(true)}>DriverLogin</button>
+          <DriverLogin trigger={Dvisible} setTrigger={setDvisible}>
+           
+          </DriverLogin>
+        </li>
+        
+        <li>
+          <button className='Userl' onClick={()=>setVisible(true)}>UserLogin</button>
           <Login trigger={Visible} setTrigger={setVisible}>
            
           </Login>
