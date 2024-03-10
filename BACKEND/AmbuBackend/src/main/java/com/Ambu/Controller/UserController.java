628b567c2929fee1203070d6821c500948c4b17d
Mohit Singh Deo <mohitdeo25@gmail.com>
2024-03-10 18:06:27 +0530

    backend file updated

--- BACKEND/AmbuBackend/src/main/java/com/Ambu/Controller/UserController.java
+++ BACKEND/AmbuBackend/src/main/java/com/Ambu/Controller/UserController.java
@@ -1,7 +1,9 @@
 package com.Ambu.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,20 +13,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.Ambu.Entity.User;
 import com.Ambu.Service.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/RegisterUser")
 public class UserController {
+	
+	ObjectMapper objMapper;
 
 	@Autowired
 	UserService userService;
 
 	@PostMapping("/save")
-	public ResponseEntity<String> save(@RequestBody User userData) {
+	public ResponseEntity<String> save(@RequestBody User userData) throws JsonProcessingException {
 
 		userService.save(userData);
-		return ResponseEntity.status(HttpStatus.OK).body(userData.toString());
+		String respJson = objMapper.writeValueAsString(userData);
+		
+		return ResponseEntity.status(HttpStatus.OK).body(respJson);
 
 	}
 
--- BACKEND/AmbuBackend/src/main/java/com/Ambu/Service/UserService.java
+++ BACKEND/AmbuBackend/src/main/java/com/Ambu/Service/UserService.java
@@ -1,6 +1,7 @@
 package com.Ambu.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.Ambu.Entity.User;
@@ -11,11 +12,19 @@ public class UserService {
 
 	@Autowired
 	UserRepository userRepo;
+	@Autowired
+	PasswordEncoder passwordEncoder;
 
 	public void save(User userData) {
-		userRepo.save(userData);
-		
+		User newUser = new User();
+		newUser.setUsername(userData.getUsername());
+		newUser.setEmail(userData.getEmail());
+		newUser.setDob(userData.getDob());
+		newUser.setPhoneNo(userData.getPhoneNo());
+		newUser.setPassword(passwordEncoder.encode(userData.getPassword()));
+
+		userRepo.save(newUser);
+
 	}
-	
-	
+
 }
